b9e492acd9dc52e6641fa4cab83a0a90c6accb24
tle <thomas.tri.le@gmail.com>
2019-01-09 11:09:27 -0500

    Removed http request from sendMessage block

--- js/background.js
+++ js/background.js
@@ -6,12 +6,17 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.sendMessage(activeTab.id,
             {
                 "message": "clicked_browser_action"
-
                 // request to make API call
+                });
+
+        chrome.browserAction.onClicked.addListener(function() { 
+  			alert('What should we name the worm? I was thinking Senior is a good, wise name'); 
+
+  			// request to make API call
                 var request = new XMLHttpRequest();
                 request.open('GET', 'hhttps://mercury.postlight.com/parser?url=' + window.location.href);
                 request.setRequestHeader('Content-Type', 'application/json');
                 request.setRequestHeader('x-api-key', 'Rp4Axlhwp4u5NFIM5wCkrjsNt2UgBXBnL9sGyX7f');
-            });
+			})
     });
 });
