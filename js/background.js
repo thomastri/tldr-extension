01f15a1039937555cfd888948bc3edcb6d150359
Alex <akarp@comscore.com>
2019-01-16 18:23:40 -0500

    Pass url to main from background. Remove old code, update flow.

--- js/background.js
+++ js/background.js
@@ -4,7 +4,8 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {
-            "message": "clicked_browser_action"
+            "action": "clicked_browser_action",
+            "url": activeTab.url
         });
 
     });
