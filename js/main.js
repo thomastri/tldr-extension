f263359c9c46da6a4002d1a7f2530b424505b8e3
Alex <akarp@comscore.com>
2019-01-16 19:25:06 -0500

    Migrate summarize api call to callback function.

--- js/main.js
+++ js/main.js
@@ -4,26 +4,35 @@ chrome.runtime.onMessage.addListener(
             const url = message.url;
             console.log(url);
 
-            // request to make API call
-            const request = new XMLHttpRequest();
-            request.open('GET', 'https://mercury.postlight.com/parser?url=' + url);
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.setRequestHeader('x-api-key', 'Rp4Axlhwp4u5NFIM5wCkrjsNt2UgBXBnL9sGyX7f');
-
-            request.onload = function () {
-                const data = JSON.parse(this.response);
-                // Strips HTML from the content
-                //console.log(data.content.replace(/<(?:.|\n)*?>/gm, ''));
-            };
-            request.send();
-
-            const awsRequest = new XMLHttpRequest();
-            awsRequest.open('GET', 'https://jldsffc2v5.execute-api.us-east-1.amazonaws.com/prod/tldrService');
-            awsRequest.setRequestHeader('x-api-key', '7hsKqeBVCv5xxfGu6IRl39jJf4ZWMJ1079Mfr2Wu');
-
-            awsRequest.send();
-            console.log(awsRequest);
+            // since my pea brain can't understand callbacks we're gonna have this shit for the time being
+            summarize(url, function(result) {
+              console.log(result);
+              //reduce();
+            });
 
         }
     }
 );
+
+function summarize(url, callback) {
+    // request to make API call
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://mercury.postlight.com/parser?url=' + url);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('x-api-key', 'Rp4Axlhwp4u5NFIM5wCkrjsNt2UgBXBnL9sGyX7f');
+
+    //var data;
+    request.onload = function () {
+        callback(JSON.parse(this.response).content.replace(/<(?:.|\n)*?>/gm, ''));
+    };
+    request.send();
+}
+
+function reduce() {
+    const awsRequest = new XMLHttpRequest();
+    awsRequest.open('GET', 'https://jldsffc2v5.execute-api.us-east-1.amazonaws.com/prod/tldrService');
+    awsRequest.setRequestHeader('x-api-key', '7hsKqeBVCv5xxfGu6IRl39jJf4ZWMJ1079Mfr2Wu');
+
+    awsRequest.send();
+    console.log(awsRequest);
+}
