9185a7cd6a7a3958c5a2b834cb98dc7dd5c8fd50
tle <thomas.tri.le@gmail.com>
2019-01-18 10:50:26 -0500

    Changed LSA to Lex_rank

--- sumy-dev/sumy/tldr.py
+++ sumy-dev/sumy/tldr.py
@@ -4,17 +4,17 @@ from __future__ import division, print_function, unicode_literals
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
 
 LANGUAGE = "english"
-SENTENCES_COUNT = 2
+SENTENCES_COUNT = 7
 
 
 if __name__ == "__main__":
-    url = "https://www.nytimes.com/2019/01/16/technology/microsoft-affordable-housing-seattle.html"
+    url = "https://blog.toggl.com/toggl-chrome-growth-hacking/"
     parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
     # or for plain text files
     # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
